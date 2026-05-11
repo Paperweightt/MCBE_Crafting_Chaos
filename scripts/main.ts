d4be63159d5740b03de1096b347c0cfcf2c007f8
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-05-10 21:42:25 -0400

    feat: spawn in player with a book

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,1 +1,12 @@
-import { system, world } from "@minecraft/server";
+import { EntityComponentTypes, ItemStack, world } from "@minecraft/server";
+import { MinecraftItemTypes } from "@minecraft/vanilla-data";
+
+world.afterEvents.playerSpawn.subscribe((data) => {
+  const { initialSpawn, player } = data;
+  if (!initialSpawn) return;
+  const container = player.getComponent(EntityComponentTypes.Inventory)?.container;
+
+  if (!container) return;
+  const item = new ItemStack(MinecraftItemTypes.WritableBook);
+  container.addItem(item);
+});
