d63dd2f9d8491c7b849801d6d890888a461ec00b
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-05-11 21:05:02 -0400

    fix: make sure its first time loading in

--- scripts/main.ts
+++ scripts/main.ts
@@ -3,10 +3,12 @@ import { MinecraftItemTypes } from "@minecraft/vanilla-data";
 
 world.afterEvents.playerSpawn.subscribe((data) => {
   const { initialSpawn, player } = data;
-  if (!initialSpawn) return;
+  if (!initialSpawn || player.getDynamicProperty("has_book")) return;
   const container = player.getComponent(EntityComponentTypes.Inventory)?.container;
 
   if (!container) return;
   const item = new ItemStack(MinecraftItemTypes.WritableBook);
+
   container.addItem(item);
+  player.setDynamicProperty("has_book", true);
 });
